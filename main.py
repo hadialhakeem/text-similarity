7181ca4fba1fc5ffb7eba92f56d1b3052ae06224
Hadi <hadinawar@hotmail.com>
2020-07-20 22:53:42 -0400

    switched entire code base to match tensorflow v2 documentaion

--- main.py
+++ main.py
@@ -1,63 +1,55 @@
+import tensorflow as tf
 import tensorflow_hub as hub
 import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+import seaborn as sns
+import os
+import re
+import pandas as pd
+
+from absl import logging
+
+embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 
 
-def heatmap(x_labels, y_labels, values):
-    fig, ax = plt.subplots()
-    im = ax.imshow(values)
-
-    # We want to show all ticks...
-    ax.set_xticks(np.arange(len(x_labels)))
-    ax.set_yticks(np.arange(len(y_labels)))
-    # ... and label them with the respective list entries
-    ax.set_xticklabels(x_labels)
-    ax.set_yticklabels(y_labels)
-
-    # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right", fontsize=10,
-             rotation_mode="anchor")
-
-    # Loop over data dimensions and create text annotations.
-    for i in range(len(y_labels)):
-        for j in range(len(x_labels)):
-            text = ax.text(j, i, "%.2f" % values[i, j],
-                           ha="center", va="center", color="w",
-                           fontsize=6)
-
-    fig.tight_layout()
-    plt.show()
+def plot_similarity(labels, features, rotation):
+    corr = np.inner(features, features)
+    sns.set(font_scale=1.2)
+    g = sns.heatmap(
+        corr,
+        xticklabels=labels,
+        yticklabels=labels,
+        vmin=0,
+        vmax=1,
+        cmap="YlOrRd")
+    g.set_xticklabels(labels, rotation=rotation)
+    g.set_title("Semantic Textual Similarity")
 
 
-module_url = "https://tfhub.dev/google/universal-sentence-encoder/1?tf-hub-format=compressed"
+def run_and_plot(messages_):
+    message_embeddings_ = embed(messages_)
+    plot_similarity(messages_, message_embeddings_, 90)
 
-# Import the Universal Sentence Encoder's TF Hub module
-embed = hub.load(module_url)
 
-# sample text
 messages = [
     # Smartphones
+    "I like my phone",
     "My phone is not good.",
     "Your cellphone looks great.",
 
     # Weather
     "Will it snow tomorrow?",
     "Recently a lot of hurricanes have hit the US",
+    "Global warming is real",
 
     # Food and health
     "An apple a day, keeps the doctors away",
     "Eating strawberries is healthy",
+    "Is paleo better than keto?",
+
+    # Asking about age
+    "How old are you?",
+    "what is your age?",
 ]
 
-similarity_input_placeholder = tf.placeholder(tf.string, shape=(None))
-similarity_message_encodings = embed.signatures['default'](similarity_input_placeholder)
-with tf.Session() as session:
-    session.run(tf.global_variables_initializer())
-    session.run(tf.tables_initializer())
-    message_embeddings_ = session.run(similarity_message_encodings, feed_dict={similarity_input_placeholder: messages})
-
-    corr = np.inner(message_embeddings_, message_embeddings_)
-    print(corr)
-    #heatmap(messages, messages, corr)
+run_and_plot(messages)
