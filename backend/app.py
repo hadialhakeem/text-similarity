e1e020cb7c7e0142cc3880947d4661b2b7eb2393
Hadi <hadinawar@hotmail.com>
2020-07-21 19:03:53 -0400

    rewrote flask app to be much shorter for my purposes

---
+++ backend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
