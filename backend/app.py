7868267d7d541588019c6bd4e71c39b0663692c6
Hadi <hadinawar@hotmail.com>
2020-07-22 13:20:00 -0400

    implemented cors properly

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,23 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
-#from backend.compute_similarity import embed
+from flask_cors import CORS
+
+# from backend.compute_similarity import embed
+
 app = Flask(__name__)
-app = CORS(app)
+CORS(app)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def helloWorld():
+    return "Hello, cross-origin-world!"
 
 
 @app.route("/compute", methods=['POST'])
 def compute_similarity():
     data = request.get_json(force=True)
     print(type(data))
-    #response = embed(data)
+    # response = embed(data)
     return jsonify(data)
-    pass
 
 
 if __name__ == "__main__":
