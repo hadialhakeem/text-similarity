c8c495b8acd6f5dc5b2737a22cb886d36ebd72bd
Hadi <hadinawar@hotmail.com>
2020-07-22 16:22:18 -0400

    refactored creating a text field into its own function

--- frontend/main.js
+++ frontend/main.js
@@ -5,26 +5,10 @@ let removeButton = document.getElementsByClassName('remove')
 let textFieldNum = 2;
 
 addTextButton.addEventListener('click', function(event){
-    textFieldNum += 1;
-    console.log("added")
-
-    // 1. Create <div> element
-    let div = document.createElement('div');
-    let textField = document.getElementById("textField");
-
-    // 2. Declare class
-    div.className = 'form-group';
-
-    // 3. Fill it with the content
-    div.innerHTML = "<label>" + 'String ' + textFieldNum.toString() + "</label>" +
-            "<textarea class='form-control' rows='4'></textarea>" +
-            "<button type='button' class='btn btn-danger mt-1'> Remove </button>";
-
-    textField.append(div)
-
+    createTextField()
 })
 
-heatmapButton.addEventListener('submit', function(event){
+heatmapButton.addEventListener('click', function(event){
    event.preventDefault()
     const apiUrl = 'http://127.0.0.1:5000/compute'
 
@@ -40,3 +24,22 @@ heatmapButton.addEventListener('submit', function(event){
 removeButton.addEventListener('click', function(event){
 
 })
+
+function createTextField(){
+    textFieldNum += 1;
+    console.log("added")
+
+    // 1. Create <div> element
+    let div = document.createElement('div');
+    let textField = document.getElementById("textField");
+
+    // 2. Declare class
+    div.className = 'form-group';
+
+    // 3. Fill it with the content
+    div.innerHTML = "<label><h5>" + 'String ' + textFieldNum.toString() + "</h5></label>" +
+        "<textarea class='form-control' rows='4'></textarea>" +
+        "<button type='button' class='btn btn-danger mt-1'> Remove </button>";
+
+    textField.append(div)
+}
